84ecdde2ccbe81a2237a44eae280eaa58d941a2c
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2025-11-02 23:39:00 +0500

    echo bot done

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
-# TODO: ZARUR ISHLAR CHIQAN SABAB BOT VAQTIDA QILIN MAD. 02/11/2025. 17:00 DA BOT TOLIQ TUGAYDI 
 import requests
 import time
 
-from config import TOKEN
+from config import TOKEN, FILE_NAME
 
 # =============Telegram API======================
 TG_BOT_URL = f'https://api.telegram.org/bot{TOKEN}'
 GetUpdates = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 SendMessange = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-
+SendPhoto = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+SendSticker = f'https://api.telegram.org/bot{TOKEN}/sendSticker'
+SendAnimation = f'https://api.telegram.org/bot{TOKEN}/sendAnimation'
+SendVoice = f'https://api.telegram.org/bot{TOKEN}/sendVoice'
+SendVideo = f'https://api.telegram.org/bot{TOKEN}/sendVideo'
+SendLocation = f'https://api.telegram.org/bot{TOKEN}/sendLocation'
+SendDocument = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
 
 
 
@@ -18,33 +23,84 @@ def get_updates(offset: int | None, limit: int = 100):
 
 def send_messange(chat_id: int | str, text: str):
     requests.get(SendMessange, params={'chat_id': chat_id, 'text': text})
+    
 
+def send_photo(chat_id: str | int, file_id: str):
+    requests.get(SendPhoto, params={'chat_id': chat_id, 'photo': file_id})
+
+
+def send_sticker(chat_id: str | int, file_id: str):
+    requests.get(SendSticker, params={'chat_id': chat_id, 'sticker': file_id})
+
+
+def send_animation(chat_id: str | int, file_id: str):
+    requests.get(SendAnimation, params={'chat_id': chat_id, 'animation': file_id})
+
+
+def send_voice(chat_id: str | int, file_id: str):
+    requests.get(SendVoice, params={'chat_id': chat_id, 'voice': file_id})
+
+
+def send_video(chat_id: str | int, file_id: str):
+    requests.get(SendVideo, params={'chat_id': chat_id, 'video': file_id})
+
+
+def send_location(chat_id: str | int, latitude: float, longitude: float):
+    requests.get(SendLocation, params={'chat_id': chat_id, 'latitude': latitude, 'longitude': longitude})
+
+
+def send_document(chat_id: str | int, file_id: str):
+    requests.get(SendDocument, params={'chat_id': chat_id, 'document': file_id})
+
+
+def save_db(user: dict):
+    with open(FILE_NAME, "w") as f:
+        pass
+    
 offset = None
 
 while True:
     for update in get_updates(offset):    
-        get_chat_id = update['message']['chat']['id']
         update_id = update['update_id']
-        first_name = update['message']['from']['first_name']
-        username = update['message']['from']['username']
         
-        if 'text' in update['message']:
-            user_text = update['message']['text']
-            text = user_text
-            if user_text == '/start':
-                text = 'salom meni botimga xosh kelib siz'
-            send_messange(get_chat_id, text)
-        elif 'photo' in update['message']:
-            print(update)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        if 'message' in update:
+            get_chat_id = update['message']['chat']['id']
+            first_name = update['message']['from']['first_name']
+            username = update['message']['from']['username']
+            
+            if 'text' in update['message']:
+                user_text = update['message']['text']
+                text = user_text
+                if user_text == '/start':
+                    text = 'salom meni botimga xosh kelib siz'
+                    try:
+                        with open(FILE_NAME) as f:
+                            pass
+                    except:
+                        pass
+                
+                send_messange(get_chat_id, text)
+            elif 'photo' in update['message']:
+                get_file_id = update['message']['photo'][-1]['file_id']
+                send_photo(get_chat_id, get_file_id)
+            elif 'sticker' in update['message']:
+                get_sticker_id = update['message']['sticker']['file_id']
+                send_sticker(get_chat_id, get_sticker_id)
+            elif 'animation' in update['message']:
+                get_animation_id = update['message']['animation']['file_id']
+                send_animation(get_chat_id, get_animation_id)
+            elif 'voice' in update['message']:
+                get_voice_id = update['message']['voice']['file_id']
+                send_voice(get_chat_id, get_voice_id)
+            elif 'video' in update['message']:
+                get_video_id = update['message']['video']['file_id']
+                send_video(get_chat_id, get_video_id)
+            elif 'location' in update['message']:
+                get_latitude = update['message']['location']['latitude']
+                get_longitude = update['message']['location']['longitude']
+                send_location(get_chat_id, get_latitude, get_longitude)
+            elif 'document' in update['message']:
+                get_document_id = update['message']['document']['file_id']
+                send_document(get_chat_id, get_document_id)
         
         offset = update_id + 1
